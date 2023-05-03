d102df3ec8de51eac2d8a4ec85c459e6944a6463
Shantanu Prasad <prasad.shantanu11@gmail.com>
2023-05-04 02:35:34 +0530

    DatabaseManager update

--- src/main/java/edu/symbi/aiml2021/platemate/DatabaseManager.java
+++ src/main/java/edu/symbi/aiml2021/platemate/DatabaseManager.java
@@ -1,6 +1,5 @@
 package edu.symbi.aiml2021.platemate;
-import com.sun.tools.javac.Main;
-
+import java.io.*;
 import java.util.*;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,6 +7,7 @@ import java.sql.Statement;
 
 public class DatabaseManager {
     static Scanner sc = new Scanner(System.in);
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args){
         try {
             System.out.println("Enter your MySQL server username: ");
@@ -17,8 +17,9 @@ public class DatabaseManager {
 
             DatabaseManager dm = new DatabaseManager();
             dm.createDatabase(username, password);
+            dm.createTable(username, password);
 
-            System.out.println("Enter which task you want: ");
+            System.out.println("\nEnter which task you want: ");
             System.out.println("1) Add menu item");
             System.out.println("2) Delete menu item");
             System.out.println("3) Update menu item");
@@ -29,40 +30,42 @@ public class DatabaseManager {
             switch (choice) {
                 case 1:
                     System.out.println("Enter item name: ");
-                    String name = sc.nextLine();
+                    String name = br.readLine();
                     System.out.println("Enter item price: ");
-                    int price = sc.nextInt();
+                    double price = Double.parseDouble(br.readLine());
                     System.out.println("Enter item description: ");
-                    String description = sc.nextLine();
+                    String description = br.readLine();
                     System.out.println("Enter item availability status (True or False): ");
-                    Boolean status = sc.nextBoolean();
-                    dm.insertRecord(username, password, name, price, description, status);
-                    System.out.println("Menu item successfully added: ");
+                    boolean status = Boolean.parseBoolean(br.readLine());
+                    int statusInt = status ? 1 : 0;
+                    dm.insertRecord(username, password, name, price, description, statusInt);
+                    System.out.println("Menu item successfully added. ");
                     break;
                 case 2:
                     System.out.println("Enter id of item to delete: ");
                     int id = sc.nextInt();
                     dm.deleteRecord(username, password, id);
-                    System.out.println("Menu item successfully deleted: ");
+                    System.out.println("Menu item successfully deleted ");
                     break;
                 case 3:
                     System.out.println("Enter id of item to update: ");
-                    id = sc.nextInt();
+                    id = Integer.parseInt(br.readLine());
                     System.out.println("Enter new item name: ");
-                    name = sc.nextLine();
+                    name = br.readLine();
                     System.out.println("Enter new item price: ");
-                    price = sc.nextInt();
+                    price = Double.parseDouble(br.readLine());
                     System.out.println("Enter new item description: ");
-                    description = sc.nextLine();
+                    description = br.readLine();
                     System.out.println("Enter new item availability status (True or False): ");
-                    status = sc.nextBoolean();
-                    dm.updateRecord(username, password, id, name, price, description, status);
-                    System.out.println("Menu item successfully updated: ");
+                    status = Boolean.parseBoolean(br.readLine());
+                    statusInt = status ? 1 : 0;
+                    dm.updateRecord(username, password, id, name, price, description, statusInt);
+                    System.out.println("Menu item successfully updated. ");
                     break;
             }
 
         }
-        catch (InputMismatchException e) {
+        catch (InputMismatchException | IOException e) {
             System.out.println("Invalid input");
         }
 
@@ -71,12 +74,15 @@ public class DatabaseManager {
         try (java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/", username, password);
              Statement stmt = conn.createStatement()) {
 
-            String CreateSql = "CREATE DATABASE IF NOT EXISTS " + "platemate";
+            String CreateSql = "CREATE DATABASE IF NOT EXISTS platemate";
             stmt.executeUpdate(CreateSql);
-
-            String UseSql = "Use platemate";
-            stmt.executeUpdate(UseSql);
-
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void createTable(String username, String password){
+        try (java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/platemate", username, password);
+             Statement stmt = conn.createStatement()) {
             String createTableSql = "CREATE TABLE IF NOT EXISTS menucard "
                     + "(id int primary key auto_increment,"
                     + " name varchar(20) NOT NULL,"
@@ -91,14 +97,13 @@ public class DatabaseManager {
             System.out.println(e.getMessage());
         }
     }
-
-    public static void insertRecord(String username, String password, String name, int price, String description, Boolean status) {
-        try (java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/",username, password);
+    public static void insertRecord(String username, String password, String name, double price, String description, int statusInt) {
+        try (java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/platemate",username, password);
              Statement stmt = conn.createStatement()) {
 
             // Insert values entered by user into the table
-            String insertSql = "INSERT INTO menucard (name, price, description, status) "
-                    + "VALUES ('" + name + "', '" + price + "', '" + description + "', " + status + ")";
+            String insertSql = "INSERT INTO menucard (name, price, description, status) " +
+                    "VALUES ('" + name + "', " + price + ", '" + description + "', '" + statusInt + "')";
             stmt.executeUpdate(insertSql);
 
         } catch (SQLException e) {
@@ -107,7 +112,7 @@ public class DatabaseManager {
     }
 
     public static void deleteRecord(String username, String password, int id){
-        try (java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/",username, password);
+        try (java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/platemate",username, password);
              Statement stmt = conn.createStatement()) {
 
             String DeleteSql = "DELETE FROM menucard WHERE id= " + id;
@@ -118,18 +123,18 @@ public class DatabaseManager {
         }
     }
 
-    public static void updateRecord(String username, String password, int id, String name, int price, String description, Boolean status){
-        try (java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/",username, password);
+    public static void updateRecord(String username, String password, int id, String name, double price, String description, int statusInt) {
+        try (java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/platemate", username, password);
              Statement stmt = conn.createStatement()) {
 
-            String UpdateSql = "UPDATE menucard SET "
-                    + "name = " + name
-                    + "price = " + price
-                    + "description = " + description
-                    + "status = " + status
-                    + "WHERE id = " + id;
+            String updateSql = "UPDATE menucard SET "
+                    + " name = '" + name + "', "
+                    + " price = " + price + ", "
+                    + " description = '" + description + "', "
+                    + " status = " + statusInt
+                    + " WHERE id = " + id;
 
-            stmt.executeUpdate(UpdateSql);
+            stmt.executeUpdate(updateSql);
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
