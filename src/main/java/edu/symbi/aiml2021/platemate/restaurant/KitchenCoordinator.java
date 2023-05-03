1ab77f5bbab512531d422c0f07acf177c1fb9754
smritisinha99 <ismisinha@gmail.com>
2023-05-03 20:00:50 +0530

    Update

--- src/main/java/edu/symbi/aiml2021/platemate/restaurant/KitchenCoordinator.java
+++ src/main/java/edu/symbi/aiml2021/platemate/restaurant/KitchenCoordinator.java
@@ -24,7 +24,7 @@ public class KitchenCoordinator extends Employee  implements IKitchenCoordinator
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        order.getOrderDetail().setStatus("Ready for Serving"); //Print status as serving
+        order.getOrderDetail().setStatus("Ready for Serving");
         try {
             sleep(1000);
         } catch (InterruptedException e) {
